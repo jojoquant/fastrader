6d7909c9672c22243d7ab2722015cdc3dc6e25d1
fangyang_gpu <fangyang.jing@hotmail.com>
2020-10-19 00:33:42 +0800

    [Add] 新增 akshare 服务

---
+++ microservices/akshare/__init__.py
@@ -0,0 +1,10 @@
+# !/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+# @Datetime : 2020/10/15 3:01
+# @Author   : Fangyang
+# @Software : PyCharm
+
+
+if __name__ == '__main__':
+    pass
---
+++ microservices/akshare/stock/__init__.py
@@ -0,0 +1,10 @@
+# !/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+# @Datetime : 2020/10/15 3:02
+# @Author   : Fangyang
+# @Software : PyCharm
+
+
+if __name__ == '__main__':
+    pass
